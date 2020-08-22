959779c5117bebdaf707a5d036e094757d81e9ea
Natalie Wilson <nataliewilson519@gmail.com>
2020-08-22 12:55:04 -0700

    Added Home and Projects components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavMenu from './components/NavMenu.js'
+import Home from './components/Home.js'
 
 function App() {
   return (
     <div className="App">
       <NavMenu />
+      <Home />
     </div>
   );
 }
