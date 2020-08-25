89db36bf7c80dba921c87bc12f6f06cace772d82
Natalie Wilson <nataliewilson519@gmail.com>
2020-08-24 19:12:45 -0700

    Misc CSS changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,8 @@ function Home() {
     return(
         <div className="intro-text">
             <p>
-            Tell me, O muse, of that ingenious hero who travelled far and wide after he had sacked the famous town of Troy. Many cities did he visit, and many were the nations with whose manners and customs he was acquainted; moreover he suffered much by sea while trying to save his own life and bring his men safely home; but do what he might he could not save his men, for they perished through their own sheer folly in eating the cattle of the Sun-god Hyperion; so the god prevented them from ever reaching home. Tell me, too, about all these things, O daughter of Jove, from whatsoever source you may know them.
+                Hello! My name is Natalie Wilson. I am a recent UC Santa Cruz graduate located in the San Francisco Bay Area.
+                I'm currently looking for Software Engineering and Software Developer roles.
             </p>
         </div>  
     );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,10 +10,9 @@ function Projects() {
 
         <div className="project-title">
             <p> SOS WA</p>
-                <img src={Clusters} className="photo" alt="clusters"></img>
-                
+            <img src={Clusters} className="photo" alt="clusters"></img>    
             <div className="sos-wa">
-            <p>
+                <p>
                 With a team of my peers for our senior Capstone project,
                 we collaborated with the UCSC Long Marine Lab to create a 
                 data visualization web app to help them track marine mammal strandings,
@@ -26,13 +25,12 @@ function Projects() {
                 The stranding map allows for the lab to view many data points simutatniosuly,
                 highlight areas of interest, and see how the data shifts overtime.
 
-
-
                 <a href={"http://lmlstrandingmap.herokuapp.com/"}> Check it out here! </a> 
             </p>
             </div>
             </div>
-        </div>
+
+            </div>
     );
 }
 
