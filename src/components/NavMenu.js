55597c8be312c74447ea40bcdefadcff11bbbcf1
Natalie Wilson <nataliewilson519@gmail.com>
2020-08-25 17:25:46 -0700

    Wavy header!

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -3,6 +3,8 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import "./NavMenu.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Resume from "./Resume.pdf";
+
 
 function NavMenu() {
 
@@ -19,8 +21,19 @@ function NavMenu() {
             <Nav.Link href="/" >Home</Nav.Link>
             <Nav.Link href="#projects" id="projects-id">Projects</Nav.Link>
             <Nav.Link href="/">Contact</Nav.Link>
+            <Nav.Link href={Resume} target="_blank">Resume</Nav.Link>
         </Navbar.Collapse>
       </Navbar>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+
+    <path fill="url(#MyGradient" d="M0,224L40,197.3C80,171,160,117,240,85.3C320,53,400,43,480,64C560,85,640,139,720,170.7C800,203,880,213,960,186.7C1040,160,1120,96,1200,69.3C1280,43,1360,53,1400,58.7L1440,64L1440,0L1400,0C1360,0,1280,0,1200,0C1120,0,1040,0,960,0C880,0,800,0,720,0C640,0,560,0,480,0C400,0,320,0,240,0C160,0,80,0,40,0L0,0Z"></path>
+    <defs>
+    <linearGradient id="MyGradient">
+          <stop offset="5%" stop-color="#f0027f" />
+          <stop offset="95%" stop-color="#754895" />
+        </linearGradient>
+    </defs>
+</svg>
     </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,22 +10,29 @@ function Projects() {
 
         <div className="project-title">
             <p> SOS WA</p>
-            <img src={Clusters} className="photo" alt="clusters"></img>    
             <div className="sos-wa">
+            <img src={Clusters} className="photo" alt="clusters"></img>    
+
                 <p>
-                With a team of my peers for our senior Capstone project,
-                we collaborated with the UCSC Long Marine Lab to create a 
+                For my senior Capstone project, my team collaborated with the UCSC Long Marine Lab (LML) to create a 
                 data visualization web app to help them track marine mammal strandings,
                 an event where a marine mammal is washed ashore, living or deceased.
+
+                <br />
+                <br />
+
                 Before we collaborated with the LML to create the stradning map, they predominately
-                perfomed data visualization and anlysis with tables and graphs. However, these methods
+                perfomed data visualization and anlysis with tables and graphs. These methods
                 don't provide a way to visualize geospatial data which is an important apsect to studying
-                marine mammal strandings.
+                marine mammal strandings, so we decided to create the stranding map.
+               
+                <br />
+                <br />
 
                 The stranding map allows for the lab to view many data points simutatniosuly,
-                highlight areas of interest, and see how the data shifts overtime.
+                highlight areas of interest, and see how the data shifts overtime. Check it out 
 
-                <a href={"http://lmlstrandingmap.herokuapp.com/"}> Check it out here! </a> 
+                <a href={"http://lmlstrandingmap.herokuapp.com/"} className="link" target="_blank"> here! </a> 
             </p>
             </div>
             </div>
