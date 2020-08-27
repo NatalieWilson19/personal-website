a69fcbb5142c69b829f71800b131568f77482de0
Natalie Wilson <nataliewilson519@gmail.com>
2020-08-26 18:04:47 -0700

    Changed projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,69 +10,92 @@ import Link from '..\\cute-link.png'
 /* TODO: Make all padding uniform, change photo of SOS-WA to be of just heatmap, more zoomed in*/
 /*<a target="_blank" href="https://icons8.com/icons/set/link">Link icon</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a> */
 function Projects() {
-    return(
+    return (
 
         <div className="section-title" id={'projects'}>
             <p> PROJECTS</p>
 
-        <div className="project-title">
+            <div className="project-title">
 
-            <a href="https://github.com/lmlstrandingnetwork/lml-stranding-map" target="_blank" rel="noopener noreferrer">
-            <img src={GitHubMark} className="github-mark" alt="github-mark"></img>
-            </a>
-            <a href="http://lmlstrandingmap.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-            <img src={Link} className="link-logo" alt="link"></img>
-            </a>
-            <p> Long Marine Lab Stranding Map</p>
-            <div className="project-subtitle">
+                <a href="https://github.com/lmlstrandingnetwork/lml-stranding-map" target="_blank" rel="noopener noreferrer">
+                    <img src={GitHubMark} className="github-mark" alt="github-mark"></img>
+                </a>
+                <a href="http://lmlstrandingmap.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                    <img src={Link} className="link-logo" alt="link"></img>
+                </a>
+                <p> Long Marine Lab Stranding Map</p>
+                <div className="project-subtitle">
 
-                <p> Data Visualization Web App
+                    <p> Data Visualization Web App
                     <br />
                 Urbica React Map GL, Firebase, Algolia, Heroku </p>
-            <div className="sos-wa">
-           {/* <img src={Clusters} className="sos-photo" alt="clusters"></img>  */}
+                    <div className="description">
 
-                <p>
-                For my senior Capstone project, my team collaborated with the UCSC Long Marine Lab (LML) to create a 
-                data visualization web app to help them track marine mammal strandings,
-                an event where a marine mammal is washed ashore, living or deceased.
+                        <p>
+                            For my senior Capstone project, my team collaborated with the UCSC Long Marine Lab (LML) to create a
+                            data visualization web app to help them track marine mammal strandings,
+                            an event where a marine mammal is washed ashore, living or deceased.
 
                 <br />
-                <br />
+                            <br />
 
                 Before we collaborated with the LML to create the stradning map, they predominately
                 perfomed data visualization and anlysis with tables and graphs. These methods
                 don't provide a way to visualize geospatial data which is an important apsect to studying
                 marine mammal strandings, so we decided to create the stranding map.
-               
+
                 <br />
-                <br />
+                            <br />
 
                 The stranding map allows for the lab to view many data points simutatniosuly,
-                highlight areas of interest, and see how the data shifts overtime. Check it out 
+                highlight areas of interest, and see how the data shifts overtime.
+                        </p>
 
-                <a href={"http://lmlstrandingmap.herokuapp.com/"} className="link" target="_blank" rel="noopener noreferrer"> here! </a> 
-            </p>
-            
-            </div>
+                    </div>
+                </div>
             </div>
 
-            </div>
 
             <div className="project-title">
+                <a href="https://github.com/paulyakovlev/pollenplanter" target="_blank" rel="noopener noreferrer">
+                    <img src={GitHubMark} className="github-mark" alt="github-mark"></img>
+                </a>
+                <p> Save Our Species (SOS) </p>
 
-            <p> PollenPlanter</p>
+                <div className="project-subtitle">
+                    <p> iOS App
+                        <br />
+                        Swift, Firebase </p>
 
-            <div className="pp">
-            {/*<img src={PollenPlanter} className="pp-photo" alt="pollen"></img> */}
+                    <div className="description">
+                        <p> SOS is an iOS app made with the intention of streamlining the
+                            process of reporting a marine mammal stranding. The app is written in Swift
+                            and uses Firebase as the database.
 
-                <p>
+                </p>
 
-                    Won first place in the EarthHacks category at CruzHacks 2020.
+                    </div>
 
+                </div>
+                </div>
+
+            <div className="project-title">
+                <a href="https://github.com/paulyakovlev/pollenplanter" target="_blank" rel="noopener noreferrer">
+                    <img src={GitHubMark} className="github-mark" alt="github-mark"></img>
+                </a>
+                <p> PollenPlanter</p>
+
+                <div className="project-subtitle">
+                    <p> Mobile App
+                        <br /> 
+                        React Native </p>
+
+                    <div className="description">
+
+                        <p>
+                            Won first place in the EarthHacks category at CruzHacks 2020.
                     <br />
-                    <br />
-
+                            <br />
                     This is a Mobile App that allows the user to get suggestions on what to plant in their
                     garden to help support their native pollinators. Most people are aware of the fact that
                     the bees are dying and a primary reason for this is their dwindling habitat as our world
@@ -80,12 +103,15 @@ function Projects() {
                     we can all do to help support the bees.
 
                     https://devpost.com/software/pollenplanter
-                    
 
                 </p>
 
+                    </div>
 
-            </div>
+
+
+
+                </div>
             </div>
         </div>
 
