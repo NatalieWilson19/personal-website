236ddfc7cb34d609a0514c4474b43d99637ceec8
Natalie Wilson <nataliewilson519@gmail.com>
2020-08-28 16:35:15 -0700

    Update colors (final)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,8 +28,8 @@ function Contact() {
         <path fill="url(#MyGradient2" d="M0,192L48,197.3C96,203,192,213,288,181.3C384,149,480,75,576,69.3C672,64,768,128,864,154.7C960,181,1056,171,1152,149.3C1248,128,1344,96,1392,80L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         <defs>
           <linearGradient id="MyGradient2">
-            <stop offset="0%" stop-color="#ccffdf" />
-            <stop offset="100%" stop-color="#469b73" />
+            <stop offset="0%" stop-color="#ACF6CA" />
+            <stop offset="100%" stop-color="#3C9169" />
             </linearGradient>
           </defs>
         </svg>
